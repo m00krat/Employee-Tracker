790f141cb3003d4d8462772a8808a8c80294ea12
m00krat <prevostm17@yahoo.com>
2023-08-12 15:40:33 -0400

    more setup

---
+++ server.js
@@ -0,0 +1,41 @@
+
+const inquirer = require('inquirer');
+
+
+const viewDepartments = (req, res) => {
+  
+};
+
+const viewRoles = (req, res) => {
+  
+};
+
+const viewEmployees = (req, res) => {
+  
+};
+
+const addDepartment = async (req, res) => {
+  
+};
+
+const addEmployee = async (req, res) => {
+  
+};
+
+const addRole = async (req, res) => {
+  
+};
+
+const updateEmployeeRole = async (req, res) => {
+  
+};
+
+module.exports = {
+  viewDepartments,
+  viewEmployees,
+  viewRoles,
+  addDepartment,
+  addEmployee,
+  addRole,
+  updateEmployeeRole,
+};
